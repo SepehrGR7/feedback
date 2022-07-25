b0b1f8261228bf268d4b1bc5c7d472a6f408b826
Sepehr Gorgini <agent.1378@gmail.com>
2022-07-26 03:22:50 +0430

    add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,6 +10,19 @@ const Header = ({ title, bgColor, textColor }) => {
   return (
     <header style={headerStyles}>
       <div className='navbar'>
+        <svg
+          width='50'
+          height='50'
+          viewBox='0 0 320 320'
+          fill='none'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <circle cx='160' cy='160' r='160' fill='#FF6A95' />
+          <path
+            d='M152.852 252V191.5H219.352V158.25H152.852V111H219.352V77H112.352V252H152.852Z'
+            fill='white'
+          />
+        </svg>
         <Link to='/'>
           <h1>{title}</h1>
         </Link>
